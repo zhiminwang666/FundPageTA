e07863ce4a5fbf28780b01af6b1f99a5e4a15a92
zhimin wang <zhiminwang666@gmail.com>
2021-11-25 15:43:06 +0800

    update message

--- Fundpage_automationTest/src/Message/MessageVerificationInMyInbox.java
+++ Fundpage_automationTest/src/Message/MessageVerificationInMyInbox.java
@@ -27,7 +27,7 @@ import org.testng.Reporter;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test; 
+import org.testng.annotations.Test;
 
 public class MessageVerificationInMyInbox {
 	private WebDriver driver;
